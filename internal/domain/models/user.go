23aeefa23c7feacc853ecf727dfde2caac1bfcb7
palich <rautaruukkipalich@gmail.com>
2024-04-10 01:27:01 +0500

    edit model user. add slug

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -5,6 +5,7 @@ import "time"
 type User struct {
 	ID                 int64
 	Username           string
+	Slug               string
 	HashedPass         []byte
 	CreatedAt          time.Time
 	UpdatedAt          time.Time
